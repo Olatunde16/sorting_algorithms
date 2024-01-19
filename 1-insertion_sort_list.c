167c6ee7999c8ed464a6eee57c630c15228d3911
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-01-19 08:16:46 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,26 +9,19 @@
  * @node2: The second node to swap.
  */
 
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2)
 {
-	if (node1->prev != NULL)
-		node1->prev->next = node2;
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
 	else
 		*list = node2;
-
-	if (node2->next != NULL)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
-
-	if (node1->next != NULL)
-		node1->next->prev = node1;
-
-	if (node2->prev != NULL)
-		node2->prev->next = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
 
 /**
