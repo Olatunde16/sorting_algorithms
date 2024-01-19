52a645f3af6f2d152f43243ad54b77b72d476979
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-01-19 08:28:42 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -39,7 +39,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap(array + i, array + i + 1);
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
