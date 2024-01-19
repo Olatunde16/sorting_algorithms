eb170a8c1f02d4799b7a425c2645466273206823
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-01-19 08:50:04 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,3 +14,33 @@ void swap_ints(int *xp, int *yp)
 	*xp = *yp;
 	*yp = temp;
 }
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min_index;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min_index;
+
+		if ((array + i) != min_index)
+		{
+			swap_ints(array + i, min_index);
+			print_array(array, size);
+		}
+	}
+}
