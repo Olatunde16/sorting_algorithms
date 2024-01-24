807ecb6dbe877aeb1a8e1b96c336f517f36fb243
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-01-24 19:21:24 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,17 +2,17 @@
 
 /**
  * swap - Swap two integers in an array.
- * @xp: The first integer to swap.
- * @yp: The second integer to swap.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
 
-void swap(int *xp, int *yp)
+void swap_ints(int *a, int *b)
 {
 	int tmp;
 
-	tmp = *xp;
-	*xp = *xp;
-	*xp = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -39,7 +39,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap(array + i, array + i + 1);
+				swa_intsp(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
